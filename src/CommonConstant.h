d0755ffb98b33fe57eb9c58eb0ca16b6121ad055
John <johnfox17@gmail.com>
2022-02-19 16:29:37 -0700

    Changed variable name to AVECTORSIZE

--- src/CommonConstant.h
+++ src/CommonConstant.h
@@ -4,6 +4,11 @@
 #define YLENGTH                 1
 #define XMATERIALPOINTS         41
 #define YMATERIALPOINTS         41
+#define XMATERIALPOINTSEXTENDED 47
+#define YMATERIALPOINTSEXTENDED 47
 #define HORIZON                 3.015
 #define TOTALNODES              XMATERIALPOINTS*YMATERIALPOINTS
+#define TOTALNODESEXTENDED      XMATERIALPOINTSEXTENDED*YMATERIALPOINTSEXTENDED
+#define AMATSIZE                36  //The A matrix is a 6x6 matrix
+#define AVECTORSIZE             6
 
